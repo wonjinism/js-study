a1985cb075795d14c4bf25df2661f6a8d54c4100
Simon Kim (Wonjin Kim) <wonjinism@gmail.com>
2018-11-13 12:26:33 +0900

    Do it! Javascript + jQuery Day 1
    Installing and preparation to study

---
+++ DoItJQuery/20181113/js/example.js
@@ -0,0 +1,15 @@
+document.write("Welcome to Hello World!");
+// New date();
+new Date();
+
+// document.write("hi") document.write("bye")
+document.write("hi"); document.write("bye");
+// document.write("책에 "자바스크립트는 대소문자를 구분해야 합니다."라고 나와 있다.");
+document.write('책에 "자바스크립트는 대소문자를 구분해야 합니다"라고 나와있다.');
+document.write("책에 \"자바스크립트는 대소문자를 구분해야 합니다\"라고 나와있다.");
+
+// 개행을 넣으려면 "안에 태그를 집어 넣어줘야 함."
+document.write("<p>Welcome to Hello World!");
+document.write("<h1>hi</h1>"); document.write("<h2>bye</h2>");
+document.write('책에 "자바스크립트는 대소문자를 구분해야 합니다"라고 나와있다.<br />');
+document.write("책에 \"자바스크립트는 대소문자를 구분해야 합니다\"라고 나와있다.");
